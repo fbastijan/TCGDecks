40a4ed6b46f65b28a7a9e42df2dbcb2810893766
Filip Bastijanić <59928451+filipbasti@users.noreply.github.com>
2024-03-20 13:11:21 +0100

    dodani navbar i view folder

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav
+    class="navbar navbar-expand-lg bg-body-tertiary navbar-dark"
+    style="background-color: #242582"
+  >
+    <div class="container-fluid">
+      <a class="navbar-brand" style="color: #f64c72" href="#">Tcg Decks</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="#"
+            ><router-link to="/" style="text-decoration: none; color: inherit"
+              >Home</router-link
+            ></a
+          >
+          <a class="nav-link" href="#"
+            ><router-link
+              to="/menucreator"
+              style="text-decoration: none; color: inherit"
+              >Make your menu</router-link
+            ></a
+          >
+        </div>
+
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link" aria-current="page" href="#"
+            ><router-link
+              to="/login"
+              style="text-decoration: none; color: inherit"
+              >Login</router-link
+            ></a
+          >
+          <a class="nav-link" href="#"
+            ><router-link
+              to="/register"
+              style="text-decoration: none; color: inherit"
+              >Register</router-link
+            ></a
+          >
+          <a class="nav-link" aria-current="page" href="#"
+            ><router-link
+              to="/profile"
+              style="text-decoration: none; color: inherit"
+              >Profil</router-link
+            ></a
+          >
+          <a class="nav-link" href="#">Odjava</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -20,7 +81,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2f2fa2;
 }
 </style>
